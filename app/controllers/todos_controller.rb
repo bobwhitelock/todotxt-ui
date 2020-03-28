30fb22d49092d6f5b4eb2ade8b23e35941ca3804
Bob Whitelock <bob.whitelock1@gmail.com>
2020-03-28 16:26:15 -0700

    Show todos in useful order

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,13 @@
 class TodosController < ApplicationController
   def index
     todo_file = ENV.fetch('TODO_FILE')
-    @todos = Todo::List.new todo_file
+    @todos = Todo::List.new(todo_file).sort_by do |todo|
+      [
+        # XXX Only show things due soon first?
+        todo.tags.fetch(:due, 'z'),
+        todo.priority || 'Z',
+        todo.created_on
+      ]
+    end
   end
 end

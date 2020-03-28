9c459dc272682bbe6d0511929a133c7c1801e498
Bob Whitelock <bob.whitelock1@gmail.com>
2020-03-28 15:46:53 -0700

    Add TailwindCSS
    Following instructions at
    https://medium.com/@davidteren/production-ready-tailwind-ui-tailwind-css-alpinejs-inter-typeface-for-ruby-on-rails-part-1-46f24daf1b26.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module TodotxtUi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end

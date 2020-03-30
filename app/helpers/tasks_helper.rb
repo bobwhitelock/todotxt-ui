f33337be759379c65c24a8eda0c867a1f59c33ac
Bob Whitelock <bob.whitelock1@gmail.com>
2020-03-29 17:28:03 -0700

    Highlight high priority tasks

---
+++ app/helpers/tasks_helper.rb
@@ -0,0 +1,23 @@
+module TasksHelper
+  def priority_text_class(task)
+    case task.priority
+    when 'A'
+      'text-red-600'
+    when 'B'
+      'text-orange-600'
+    when 'C'
+      'text-yellow-600'
+    end
+  end
+
+  def priority_background_class(task)
+    case task.priority
+    when 'A'
+      'bg-red-200'
+    when 'B'
+      'bg-orange-200'
+    when 'C'
+      'bg-yellow-200'
+    end
+  end
+end

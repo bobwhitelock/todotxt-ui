af24e97fab9a2193bc0a160a8935459e633e8dd2
Bob Whitelock <bob.whitelock1@gmail.com>
2020-07-01 19:59:17 +0100

    Remove verbose config comment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,17 +28,6 @@ require 'support/factory_bot'
 RSpec.configure do |config|
   config.use_active_record = true
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, type: :controller do
-  #       # ...
-  #     end
-  #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!

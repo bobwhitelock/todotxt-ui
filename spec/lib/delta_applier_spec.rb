3a2a016bf37d9356706cd2df4b11d2317b18ad60
Bob Whitelock <bob.whitelock1@gmail.com>
2020-07-15 15:18:16 +0100

    Extract RepoUtils for helpers from DeltaApplier spec
    So can use these in other tests, in particular in those for TodoRepo.

--- spec/lib/delta_applier_spec.rb
+++ spec/lib/delta_applier_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
+require 'support/repo_utils'
 
 RSpec.describe DeltaApplier do
+  include RepoUtils
+
   describe '.apply' do
     it 'handles `add` delta' do
       now = Time.local(2020, 6, 6)
@@ -187,34 +190,6 @@ RSpec.describe DeltaApplier do
         end
       end
     end
-  end
 
-  def mock_todo_repo(*tasks)
-    temp_file = Tempfile.new
-    tasks.each do |task|
-      temp_file << task << "\n"
-    end
-    temp_file.write
-    temp_file.rewind
-    TodoRepo.new(temp_file.path)
-  end
-
-  def expect_tasks_saved(todo_repo, expected_raw_tasks)
-    # Assert given tasks are present in both the given TodoRepo, and a fresh
-    # TodoRepo created for the same todo_file (i.e. these tasks have also been
-    # saved to disk).
-    expect_tasks_equal(todo_repo, expected_raw_tasks)
-    expect_tasks_equal(fresh_todo_repo(todo_repo), expected_raw_tasks)
-  end
-
-  def expect_tasks_equal(todo_repo, expected_raw_tasks)
-    expected_tasks = expected_raw_tasks.map do |raw_task|
-      Todo::Task.new(raw_task)
-    end
-    expect(todo_repo.tasks).to eq(expected_tasks)
-  end
-
-  def fresh_todo_repo(todo_repo)
-    TodoRepo.new(todo_repo.todo_file)
   end
 end

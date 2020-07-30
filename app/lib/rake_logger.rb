77342b7cfbcf9a338929d58e52587f2221d51393
Bob Whitelock <bob.whitelock1@gmail.com>
2020-07-30 23:51:13 +0100

    Add some basic logging to cron tasks (resolves #51)

---
+++ app/lib/rake_logger.rb
@@ -0,0 +1,10 @@
+
+class RakeLogger
+  # Delegate to Rails logger, but with all lines logged prefixed with full Rake
+  # task name for current task.
+  def self.method_missing(log_level, log_line)
+    task_name = Rake.application.top_level_tasks.first
+    log_line = "#{task_name}: #{log_line}"
+    Rails.logger.send(log_level, log_line)
+  end
+end

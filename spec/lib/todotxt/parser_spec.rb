e90b0a9d453f0089f9606e31347dccf9d5c922f0
Bob Whitelock <bob.whitelock1@gmail.com>
2020-08-04 00:57:11 +0100

    WIP

--- spec/lib/todotxt/parser_spec.rb
+++ spec/lib/todotxt/parser_spec.rb
@@ -204,5 +204,7 @@ RSpec.describe Todotxt::Parser do
 
   describe "space" do
     it { expect(subject.space).to parse(" ") }
+    # XXX Why doesn't this work?
+    # it { expect(subject.space).to parse("\t") }
   end
 end

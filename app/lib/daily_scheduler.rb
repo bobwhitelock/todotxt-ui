dcd9a20a4480574e7f4737845506655b62623585
Bob Whitelock <bob.whitelock1@gmail.com>
2021-02-06 21:31:13 +0000

    Slightly refactor DailyScheduler to be more concise

--- app/lib/daily_scheduler.rb
+++ app/lib/daily_scheduler.rb
@@ -2,21 +2,19 @@ class DailyScheduler
   def self.progress(todo_repo:)
     incomplete_tasks = todo_repo.incomplete_tasks
 
-    today_tasks = incomplete_tasks.select(&:today?)
-    today_tasks.map do |task|
+    today_tasks = incomplete_tasks.select(&:today?).map { |task|
       task.unschedule(update_scheduled_tag: true)
-    end
+    }
 
     current_day = Date.today.strftime("%A")
     current_day_context = Context.from(current_day) # E.g. "@tuesday"
     new_today_tasks = incomplete_tasks.select { |task|
       contexts = task.contexts
       contexts.include?(current_day_context) || contexts.include?(Context::TOMORROW)
-    }
-    new_today_tasks.map do |task|
+    }.map { |task|
       task.contexts -= [current_day_context, Context::TOMORROW]
       task.schedule
-    end
+    }
 
     todo_repo.commit_todo_file("Progress scheduled tasks") && todo_repo.push
 

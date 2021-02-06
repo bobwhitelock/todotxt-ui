df66d1f853eba9ad66781f44d8a1ed650a41aff8
Bob Whitelock <bob.whitelock1@gmail.com>
2021-02-06 00:10:58 +0000

    Extract StubGit to own file
    So can use elsewhere.

---
+++ spec/support/stub_git.rb
@@ -0,0 +1,12 @@
+# TODO Is there a better approach to testing where this is used, that doesn't
+# require this stub?
+class StubGit
+  def config(*args)
+  end
+
+  def add(*args)
+  end
+
+  def commit(*args)
+  end
+end

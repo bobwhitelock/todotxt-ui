6fb50bde9f2370f6413d68a7b81b106b64182a6e
Bob Whitelock <bob.whitelock1@gmail.com>
2021-02-06 00:22:25 +0000

    Slightly refactor DailyScheduler

--- app/lib/daily_scheduler.rb
+++ app/lib/daily_scheduler.rb
@@ -1,15 +1,12 @@
 class DailyScheduler
   def self.progress(todo_repo:)
-    cleared_tasks = 0
-    todo_repo.incomplete_tasks
-      .select(&:today?)
-      .map do |task|
+    today_tasks = todo_repo.incomplete_tasks.select(&:today?)
+    today_tasks.map do |task|
       task.unschedule(update_scheduled_tag: true)
-      cleared_tasks += 1
     end
 
     todo_repo.commit_todo_file("Automatically clear today list") && todo_repo.push
 
-    RakeLogger.info "#{cleared_tasks} tasks cleared"
+    RakeLogger.info "#{today_tasks.length} tasks cleared"
   end
 end

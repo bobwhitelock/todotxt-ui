bobwhitelock/todotxt-ui

commit 2bbc4114219470aa6e55e0d6462c58e35d546ac5

Switch over to use own todotxt parser
Resolves https://github.com/bobwhitelock/todotxt-ui/issues/55.
Resolves https://github.com/bobwhitelock/todotxt-ui/issues/17.

app/controllers/tasks_controller.rb
@@ -8,13 +8,18 @@ class TasksController < ApplicationController
       commit: false
     )
 
-    @total_tasks = tasks.by_not_done.length
+    @total_tasks = todo_repo.incomplete_tasks.length
+
     @tasks = tasks_to_show.sort_by { |task|
+      # Each entry in comparison array should always be of the same type (or
+      # nil) for every task, otherwise this can blow up.
       [
         task.today? ? "a" : "b",
-        task.tags.fetch(:due, "z"),
+        # Compare `due` metadata values as strings, since there is no guarantee
+        # that only Dates will be used for these.
+        task.metadata.fetch(:due, "zzz").to_s,
         task.priority || "Z",
-        task.created_on || 100.years.from_now,
+        task.creation_date || 100.years.from_now,
         task.raw
       ]
     }
@@ -65,20 +70,18 @@ class TasksController < ApplicationController
 
   private
 
-  delegate :tasks, to: :todo_repo
-
   def todo_repo
     @_todo_repo ||= TodoRepo.new(Figaro.env.TODO_FILE!)
   end
 
   def tasks_to_show
-    to_show = tasks.by_not_done
+    to_show = todo_repo.incomplete_tasks
 
     filters.each do |filter|
       if filter.start_with?("+")
-        to_show = to_show.by_project(filter)
+        to_show = to_show.select { |t| t.projects.include?(filter) }
       elsif filter.start_with?("@")
-        to_show = to_show.by_context(filter)
+        to_show = to_show.select { |t| t.contexts.include?(filter) }
       end
     end
 

Gemfile
@@ -48,7 +48,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem "todo-txt"
 gem "rinku"
 gem "git"
 gem "draper"

app/lib/delta_applier.rb
@@ -20,9 +20,8 @@ class DeltaApplier
   end
 
   def apply
-    original_raw_tasks = todo_repo.raw_tasks
     handle_delta
-    save_delta_change(original_raw_tasks) if commit
+    save_delta_change if commit
   end
 
   private
@@ -76,28 +75,21 @@ class DeltaApplier
 
   def handle_schedule
     task = delta.arguments.first
-    todo_repo.replace_task(task, task.strip + " @today")
+    todo_repo.map_task(task) { |t| t.contexts += ["@today"] }
   end
 
   def handle_unschedule
     task = delta.arguments.first
-    todo_repo.replace_task(task, task.gsub(/\s+@today\s*/, " "))
+    todo_repo.map_task(task) { |t| t.contexts -= ["@today"] }
   end
 
   def handle_invalid_delta
     delta.update!(status: Delta::INVALID)
   end
 
-  def save_delta_change(original_raw_tasks)
+  def save_delta_change
     return unless commit_message
-
-    # Need to save and then reload TodoRepo when checking if tasks have
-    # changed, as `todo-txt` Gem does not change raw tasks for all changes,
-    # e.g. a completed Task keeps the same raw version until it has been
-    # saved and reloaded from disk.
-    todo_repo.tasks.save!
-    tasks_changed = TodoRepo.reload(todo_repo).raw_tasks != original_raw_tasks
-    todo_repo.commit_todo_file(commit_message) if tasks_changed
+    todo_repo.commit_todo_file(commit_message)
     delta.update!(status: Delta::APPLIED)
   end
 end

app/lib/todo_repo.rb
@@ -1,58 +1,34 @@
 class TodoRepo
-  attr_reader :todo_file
+  attr_reader :list
+  delegate :all_contexts, :all_projects, to: :list
   delegate :push, to: :repo
 
   def initialize(todo_file)
-    @todo_file = todo_file
+    @list = Todotxt::List.load(todo_file)
   end
 
-  # Create a new TodoRepo from given TodoRepo, but with tasks reloaded fresh
-  # from disk.
-  def self.reload(other_todo_repo)
-    TodoRepo.new(other_todo_repo.todo_file)
-  end
-
-  def tasks
-    @_tasks ||= Todo::List.new(todo_file)
-  end
-
-  def raw_tasks
-    tasks.map { |task| task.raw.strip }
+  def incomplete_tasks
+    list.select(&:incomplete?)
   end
 
   def add_task(raw_task)
-    tasks << Todo::Task.new(raw_task.strip)
+    list << raw_task
   end
 
   def delete_task(raw_task)
-    tasks.delete_if do |t|
-      t.raw.strip == raw_task.strip
-    end
+    list.delete_if { |t| t.raw == raw_task.strip }
   end
 
   def replace_task(old_raw_task, new_raw_task)
-    # TODO Actually replace inline rather than deleting old and then adding
-    # (i.e. appending at bottom of file) new task -
-    # https://github.com/bobwhitelock/todotxt-ui/issues/17.
-    if tasks.map { |t| t.raw.strip }.include?(old_raw_task.strip)
-      delete_task(old_raw_task)
-      add_task(new_raw_task)
-    end
+    map_task(old_raw_task) { |task| task.raw = new_raw_task }
   end
 
   def complete_task(raw_task)
-    matching_task = tasks.find { |t|
-      t.raw.strip == raw_task.strip
-    }
-    matching_task&.do!
+    map_task(raw_task, &:complete!)
   end
 
-  def all_projects
-    extract_tag(:projects)
-  end
-
-  def all_contexts
-    extract_tag(:contexts)
+  def map_task(raw_task, &block)
+    list.select { |t| t.raw == raw_task.strip }.map(&block)
   end
 
   def reset_to_origin
@@ -61,11 +37,18 @@ class TodoRepo
   end
 
   def commit_todo_file(message)
+    return false unless list.dirty?
+    list.save
     repo.add(todo_file)
     repo.commit(message)
+    true
   end
 
   private
+
+  def todo_file
+    list.file
+  end
 
   def repo
     @_repo ||=
@@ -87,9 +70,5 @@ class TodoRepo
       return path if git_path.exist?
       path = path.parent
     end
-  end
-
-  def extract_tag(tag_type)
-    tasks.flat_map(&tag_type).uniq.map { |p| p[1..-1] }
   end
 end

app/views/tasks/index.html.erb
@@ -67,7 +67,7 @@
     <div class="card justify-between <%= task.border_color_class %> <%= task.background_class %>">
       <div class="flex-grow">
         <div class="float-right px-1 py-2 text-gray-600">
-          <% task.tags.each do |tag, value| %>
+          <% task.metadata.each do |tag, value| %>
             <span class="px-1">
               <%= tag %>:<%= value %>
             </span>
@@ -79,7 +79,7 @@
 
         <div class="px-2 py-2">
           <p class="text-gray-900">
-            <%= auto_link(task.text) %>
+            <%= auto_link(task.description_text) %>
           </p>
         </div>
 
@@ -101,7 +101,7 @@
       </div>
 
       <div class="px-2 py-1 text-gray-600">
-        <%= task.created_on %>
+        <%= task.creation_date %>
       </div>
 
       <div class="flex justify-between">

lib/tasks/todotxt.rake
@@ -1,46 +1,26 @@
 namespace :todotxt do
   namespace :cron do
-    # TODO Make this nicer and DRY up with other parts of app.
     desc "Automatically clear today list and bump `scheduled` tracking"
     task clear_today_list: :environment do
       RakeLogger.info "starting"
 
       repo = TodoRepo.new(Figaro.env.TODO_FILE!)
       repo.reset_to_origin
-      tasks = repo.tasks
 
-      updates = {}
-      tasks.by_not_done.each do |task|
-        task = TaskDecorator.new(task)
-
-        if task.today?
-          scheduled = task.tags.fetch(:scheduled, 0).to_i
-
-          new_task = task
-            .raw
-            .gsub(/\s*@today\s*/, " ")
-            .gsub(/\s*scheduled:#{scheduled}\s*/, " ")
-            .strip
-          new_task += " scheduled:#{scheduled + 1}"
-
-          updates[task.raw] = new_task
-        end
+      cleared_tasks = 0
+      repo.incomplete_tasks
+        .map { |task| TaskDecorator.new(task) }
+        .select(&:today?)
+        .map do |task|
+        task.contexts -= ["@today"]
+        scheduled = task.metadata.fetch(:scheduled, 0).to_i
+        task.metadata = {**task.metadata, scheduled: scheduled + 1}
+        cleared_tasks += 1
       end
 
-      updates.each do |raw_task, new_task|
-        tasks.delete_if do |t|
-          t.respond_to?(:raw) && t.raw == raw_task
-        end
-        tasks << new_task
-      end
+      repo.commit_todo_file("Automatically clear today list") && repo.push
 
-      unless updates.empty?
-        repo.tasks.save!
-        repo.commit_todo_file("Automatically clear today list")
-        repo.push
-      end
-
-      RakeLogger.info "#{updates.length} tasks cleared"
+      RakeLogger.info "#{cleared_tasks} tasks cleared"
     end
 
     desc "Attempt to sync unapplied local Deltas to remote repo"

spec/lib/delta_applier_spec.rb
@@ -4,6 +4,22 @@ require "support/repo_utils"
 RSpec.describe DeltaApplier do
   include RepoUtils
 
+  # TODO Better approach to testing this that doesn't require this stub?
+  class StubGit
+    def config(*args)
+    end
+
+    def add(*args)
+    end
+
+    def commit(*args)
+    end
+  end
+
+  before do
+    allow(Git).to receive(:open).and_return(StubGit.new)
+  end
+
   describe ".apply" do
     it "handles `add` delta" do
       now = Time.local(2020, 6, 6)
@@ -16,7 +32,7 @@ RSpec.describe DeltaApplier do
       )
       todo_repo = mock_todo_repo("other task")
 
-      expect(todo_repo).to receive(:commit_todo_file).with("Create task(s)")
+      expect(todo_repo).to receive(:commit_todo_file).with("Create task(s)").and_call_original
       DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo)
 
       expect_tasks_saved(todo_repo, [
@@ -37,7 +53,7 @@ RSpec.describe DeltaApplier do
       )
       todo_repo = mock_todo_repo("other task", "old task")
 
-      expect(todo_repo).to receive(:commit_todo_file).with("Update task")
+      expect(todo_repo).to receive(:commit_todo_file).with("Update task").and_call_original
       DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo)
 
       expect_tasks_saved(todo_repo, ["other task", "new task"])
@@ -53,7 +69,7 @@ RSpec.describe DeltaApplier do
       )
       todo_repo = mock_todo_repo("other task", "some task")
 
-      expect(todo_repo).to receive(:commit_todo_file).with("Delete task")
+      expect(todo_repo).to receive(:commit_todo_file).with("Delete task").and_call_original
       DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo)
 
       expect_tasks_saved(todo_repo, ["other task"])
@@ -71,7 +87,7 @@ RSpec.describe DeltaApplier do
       )
       todo_repo = mock_todo_repo("other task", "2020-05-05 some task")
 
-      expect(todo_repo).to receive(:commit_todo_file).with("Complete task")
+      expect(todo_repo).to receive(:commit_todo_file).with("Complete task").and_call_original
       DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo)
 
       expect_tasks_saved(todo_repo, [
@@ -90,7 +106,7 @@ RSpec.describe DeltaApplier do
       )
       todo_repo = mock_todo_repo("other task", "some task")
 
-      expect(todo_repo).to receive(:commit_todo_file).with("Add task to today list")
+      expect(todo_repo).to receive(:commit_todo_file).with("Add task to today list").and_call_original
       DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo)
 
       expect_tasks_saved(todo_repo, ["other task", "some task @today"])
@@ -106,7 +122,7 @@ RSpec.describe DeltaApplier do
       )
       todo_repo = mock_todo_repo("other task", "some task @today @another-tag")
 
-      expect(todo_repo).to receive(:commit_todo_file).with("Remove task from today list")
+      expect(todo_repo).to receive(:commit_todo_file).with("Remove task from today list").and_call_original
       DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo)
 
       expect_tasks_saved(todo_repo, ["other task", "some task @another-tag"])
@@ -126,7 +142,7 @@ RSpec.describe DeltaApplier do
       }
       todo_repo = mock_todo_repo("other task")
 
-      expect(todo_repo).to receive(:commit_todo_file).thrice.with("Create task(s)")
+      expect(todo_repo).to receive(:commit_todo_file).thrice.with("Create task(s)").and_call_original
       DeltaApplier.apply(deltas: deltas, todo_repo: todo_repo)
 
       expect_tasks_saved(todo_repo, [
@@ -144,7 +160,7 @@ RSpec.describe DeltaApplier do
       delta = create(:delta, type: Delta::UPDATE, arguments: ["foo", "bar"])
       todo_repo = mock_todo_repo("other task")
 
-      expect(todo_repo).not_to receive(:commit_todo_file)
+      expect(todo_repo.list).not_to receive(:save)
       DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo)
 
       expect_tasks_saved(todo_repo, ["other task"])
@@ -174,7 +190,7 @@ RSpec.describe DeltaApplier do
       Delta::TYPES.each do |type|
         delta = create(:delta, type: type, arguments: ["arg"] * 10)
         todo_repo = mock_todo_repo
-        allow(todo_repo).to receive(:commit_todo_file)
+        allow(todo_repo).to receive(:commit_todo_file).and_call_original
 
         expect {
           DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo)
@@ -195,7 +211,6 @@ RSpec.describe DeltaApplier do
             arguments: ["some task", "another arg"]
           )
 
-          expect(todo_repo.tasks).not_to receive(:save!)
           expect(todo_repo).not_to receive(:commit_todo_file)
           DeltaApplier.apply(deltas: [delta], todo_repo: todo_repo, commit: false)
           expect(delta.reload).to be_unapplied

spec/lib/todo_repo_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe TodoRepo do
 
       todo_repo.replace_task("old task", "new task")
 
-      expect_tasks_saved(todo_repo, ["other task", "new task"])
+      expect(todo_repo.list.raw_tasks).to eq(["other task", "new task"])
     end
 
     it "does nothing when old task not present in repo" do
@@ -19,7 +19,7 @@ RSpec.describe TodoRepo do
 
       todo_repo.replace_task("old task", "new task")
 
-      expect_tasks_saved(todo_repo, ["other task"])
+      expect(todo_repo.list.raw_tasks).to eq(["other task"])
     end
 
     it "works when tasks include arbitrary whitespace" do
@@ -27,7 +27,7 @@ RSpec.describe TodoRepo do
 
       todo_repo.replace_task(" old task   ", "  new task  ")
 
-      expect_tasks_saved(todo_repo, ["other task", "new task"])
+      expect(todo_repo.list.raw_tasks).to eq(["other task", "new task"])
     end
   end
 end

commit e3ed918dab4009c3f52c49e126ce612085366f8e

Fix @/+ double-inserting on autocomplete (fixes #91)
Resolves issue introduced in
https://github.com/bobwhitelock/todotxt-ui/commit/2bbc4114219470aa6e55e0d6462c58e35d546ac5
- the old parser did not include the initial symbol when getting the
contexts/projects for a task, whereas mine does.

The solution is to strip this out when getting the contexts/projects to
use in the JS autocomplete, which doesn't expect this and would insert
them twice (once on triggering the autocomplete and once on successful
autocompletion).

app/controllers/tasks_controller.rb
@@ -81,7 +81,10 @@ class TasksController < ApplicationController
   alias assign_filters filters
 
   def assign_tags
-    @projects = todo_repo.all_projects
-    @contexts = (todo_repo.all_contexts + Context::SPECIAL_CONTEXTS).uniq
+    projects = todo_repo.all_projects
+    @projects = projects.map { |p| p[1...] }
+
+    contexts = todo_repo.all_contexts + Context::SPECIAL_CONTEXTS
+    @contexts = contexts.uniq.map { |p| p[1...] }
   end
 end
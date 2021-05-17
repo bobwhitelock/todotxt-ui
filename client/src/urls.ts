7a650330693011076bfcbf5dbc2a0d3ae5dbf6db
Bob Whitelock <bob.whitelock1@gmail.com>
2021-05-17 01:55:18 +0100

    Add support for editing Tasks to React app
    A lot of duplication with add page for now.

--- client/src/urls.ts
+++ client/src/urls.ts
@@ -1,2 +1,8 @@
+import { Task } from "types/Task";
+
 export const root = "/";
 export const add = "/add";
+export const edit = {
+  template: "/edit/:rawTask",
+  forTask: (task: Task) => `/edit/${task.raw}`,
+};

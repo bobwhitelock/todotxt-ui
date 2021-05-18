a16d472c50b06939f0870af41df8e760331f8965
Bob Whitelock <bob.whitelock1@gmail.com>
2021-05-18 22:45:34 +0100

    Handle initial filtering and sorting of Tasks
    Only show incomplete Tasks, and sort in the same order as in the Rails
    app.

--- client/src/types/Task.ts
+++ client/src/types/Task.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 export type Task = {
   raw: string;
   description_text: string;
@@ -13,4 +15,14 @@ export type Task = {
 
 export function isToday(task: Task): boolean {
   return task.contexts.includes("@today");
+}
+
+export function sorted(tasks: Task[]): Task[] {
+  return _.sortBy(tasks, [
+    (task) => !isToday(task),
+    "metadata.due",
+    "priority",
+    "creation_date",
+    "raw",
+  ]);
 }

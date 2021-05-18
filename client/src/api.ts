016aa61529451f49b2e35948c17e22e9c083d125
Bob Whitelock <bob.whitelock1@gmail.com>
2021-05-18 22:06:51 +0100

    Wire up Task card actions
    Also tweak `useUpdateTasks` so the updated Tasks sent in the response to
    each update will be saved, and hence visible immediately, and allow the
    `IconButton`s to display as disabled while each update is in progress.

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,20 +1,23 @@
-import { useQuery, useMutation } from "react-query";
+import { useQuery, useQueryClient, useMutation } from "react-query";
 
 import { Task } from "types/Task";
 import { DeltaType } from "types/DeltaType";
 
 const TASKS_URL = "/api/tasks";
+const TASKS_KEY = "tasks";
 
 // XXX Add generic error handling - show alert or similar
 
 export function useTasks() {
-  return useQuery<{ data: Task[] }, Error>("tasks", () =>
+  return useQuery<{ data: Task[] }, Error>(TASKS_KEY, () =>
     fetch(TASKS_URL).then((response) => response.json())
   );
 }
 
 export function useUpdateTasks(deltaType: DeltaType, deltaArguments: string[]) {
-  const mutation = useMutation(() =>
+  const queryClient = useQueryClient();
+
+  const updateTasks = () =>
     fetch(TASKS_URL, {
       method: "POST",
       headers: {
@@ -22,8 +25,11 @@ export function useUpdateTasks(deltaType: DeltaType, deltaArguments: string[]) {
         "Content-Type": "application/json;charset=UTF-8",
       },
       body: JSON.stringify({ type: deltaType, arguments: deltaArguments }),
-    })
-  );
+    }).then((response) => response.json());
+
+  const mutation = useMutation(updateTasks, {
+    onSuccess: (data) => queryClient.setQueryData(TASKS_KEY, data),
+  });
 
   const eventHandler = (event: React.SyntheticEvent) => {
     event.preventDefault();

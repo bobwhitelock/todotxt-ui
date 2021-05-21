583e3ffb21b393dd8daf28c06d0c21b9282c2cd9
Bob Whitelock <bob.whitelock1@gmail.com>
2021-05-21 14:34:39 +0100

    Log to stderr in development
    This makes all useful output immediately available and obvious, rather
    than hidden in a log file.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Log to stderr in development.
+  config.logger = Logger.new($stderr)
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 

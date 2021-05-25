0f73227661dfe94623e210002d28962b6a1985a2
Bob Whitelock <bob.whitelock1@gmail.com>
2021-05-25 02:16:55 +0100

    DRY up Add and Edit Tasks forms

---
+++ client/src/components/TaskForm.tsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import cn from "classnames";
+import { Redirect } from "react-router-dom";
+
+import * as urls from "urls";
+import { UpdateTasksMutationResult } from "api";
+import { useQueryParams, urlWithParams } from "queryParams";
+
+type Props = {
+  initialRawTask: string;
+  useUseUpdateTasksWithTasks: (rawTasks: string) => {
+    mutation: UpdateTasksMutationResult;
+    eventHandler: (event: React.SyntheticEvent) => void;
+  };
+  getSubmitButtonText: ({
+    plural,
+    loading,
+  }: {
+    plural: boolean;
+    loading: boolean;
+  }) => string;
+};
+
+function TaskForm({
+  initialRawTask,
+  useUseUpdateTasksWithTasks,
+  getSubmitButtonText,
+}: Props) {
+  const [rawTasks, setRawTasks] = useState(initialRawTask);
+  const trimmedRawTasks = rawTasks.trim();
+
+  const { mutation, eventHandler: onSubmit } =
+    useUseUpdateTasksWithTasks(rawTasks);
+
+  const params = useQueryParams();
+  if (mutation.isSuccess) {
+    return <Redirect to={urlWithParams(urls.root, params)} />;
+  }
+
+  return (
+    <form
+      className="container flex flex-col h-screen px-4 py-6 mx-auto"
+      onSubmit={onSubmit}
+    >
+      {/* XXX Add autocompletion in text area */}
+      <textarea
+        className={cn(
+          "flex-grow",
+          "w-full",
+          "border-4",
+          "border-blue-200",
+          "border-solid",
+          "rounded-lg",
+          "md:flex-grow-0",
+          "md:h-64"
+        )}
+        autoFocus={true}
+        value={rawTasks}
+        onChange={(e) => setRawTasks(e.target.value)}
+      ></textarea>
+
+      <button
+        disabled={
+          trimmedRawTasks === "" ||
+          trimmedRawTasks === initialRawTask ||
+          mutation.isLoading
+        }
+        className={cn(
+          "w-full",
+          "py-3",
+          "mt-3",
+          "mb-16",
+          "rounded-lg",
+          "text-white",
+          "bg-green-600",
+          "hover:bg-green-800",
+          "focus:bg-green-800",
+          "font-bold",
+          "cursor-pointer",
+          "disabled:bg-green-600",
+          "disabled:opacity-75",
+          "disabled:cursor-auto"
+        )}
+      >
+        {/* XXX Debounce changing text based on whether loading so doesn't flash very briefly? */}
+        {getSubmitButtonText({
+          plural: trimmedRawTasks.split("\n").length !== 1,
+          loading: mutation.isLoading,
+        })}
+      </button>
+    </form>
+  );
+}
+
+export default TaskForm;

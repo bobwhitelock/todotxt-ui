871d8aee6c875b38f551d55a526cc965b98771a7
Bob Whitelock <bob.whitelock1@gmail.com>
2021-05-30 00:12:58 +0100

    Make page titles dynamic and set via React

--- client/src/components/pages/Root.tsx
+++ client/src/components/pages/Root.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import * as Icon from "components/Icon";
 import * as urls from "urls";
@@ -13,7 +14,7 @@ import {
   getContextParams,
   getProjectParams,
 } from "queryParams";
-import { sortTasks } from "types";
+import { sortTasks, pluralizeTasks } from "types";
 
 export default function Root() {
   // XXX Handle isLoading and error
@@ -45,71 +46,85 @@ export default function Root() {
     window.scrollTo(0, document.body.scrollHeight);
   };
 
+  const filteredTasksCount = filteredTasks.length;
+  const incompleteTasksCount = incompleteTasks.length;
+  const taskOrTasks = pluralizeTasks(incompleteTasks);
+  const title =
+    filteredTasksCount === incompleteTasksCount
+      ? `${incompleteTasksCount} ${taskOrTasks}`
+      : `${filteredTasksCount}/${incompleteTasksCount} ${taskOrTasks}`;
+
   return (
-    <div className="flex flex-wrap text-lg">
-      <div className="sticky top-0 bg-gray-300 card md:static">
-        <div className="py-2">
-          {incompleteTasks.length} tasks ({filteredTasks.length} shown)
-        </div>
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
 
-        <div className="flex-grow py-2">
-          Filters:{" "}
-          <TagPills
-            tagType="project"
-            tags={projectFilters}
-            action="removeFilter"
-          />
-          <TagPills
-            tagType="context"
-            tags={contextFilters}
-            action="removeFilter"
-          />
-          <span className="text-sm">
-            {anyFilters ? (
-              <Link to="?" className="text-blue-800 hover:opacity-50">
-                Clear&nbsp;All
-              </Link>
-            ) : (
-              "(None)"
-            )}
-          </span>
-        </div>
+      <div className="flex flex-wrap text-lg">
+        <div className="sticky top-0 bg-gray-300 card md:static">
+          <div className="py-2">
+            {incompleteTasks.length} tasks ({filteredTasks.length} shown)
+          </div>
 
-        <div className="flex justify-between">
-          {/* TODO Improve styling of these icons */}
-          {/* Empty element so button gets flexed to right, and in consistent way to */}
-          {/* buttons on other cards. */}
-          <span></span>
-
-          <IconButton onClick={scrollToTop} className="md:hidden">
-            <Icon.ArrowThickUpCircle
-              backgroundClass="text-gray-500"
-              foregroundClass="text-gray-600"
+          <div className="flex-grow py-2">
+            Filters:{" "}
+            <TagPills
+              tagType="project"
+              tags={projectFilters}
+              action="removeFilter"
             />
-          </IconButton>
-
-          <IconButton onClick={scrollToBottom} className="md:hidden">
-            <Icon.ArrowThickDownCircle
-              backgroundClass="text-gray-500"
-              foregroundClass="text-gray-600"
+            <TagPills
+              tagType="context"
+              tags={contextFilters}
+              action="removeFilter"
             />
-          </IconButton>
+            <span className="text-sm">
+              {anyFilters ? (
+                <Link to="?" className="text-blue-800 hover:opacity-50">
+                  Clear&nbsp;All
+                </Link>
+              ) : (
+                "(None)"
+              )}
+            </span>
+          </div>
 
-          <Link to={urlWithParams(urls.add, params)}>
-            <IconButton>
-              <Icon.AddSquare
-                backgroundClass="text-green-300"
-                foregroundClass="text-green-600"
+          <div className="flex justify-between">
+            {/* TODO Improve styling of these icons */}
+            {/* Empty element so button gets flexed to right, and in consistent way to */}
+            {/* buttons on other cards. */}
+            <span></span>
+
+            <IconButton onClick={scrollToTop} className="md:hidden">
+              <Icon.ArrowThickUpCircle
+                backgroundClass="text-gray-500"
+                foregroundClass="text-gray-600"
               />
             </IconButton>
-          </Link>
-        </div>
-      </div>
 
-      {filteredTasks.map((task, index) => (
-        // XXX Better index than this? Need to generate and send ID from server?
-        <TaskCard task={task} key={`${index}:${task.raw}`} />
-      ))}
-    </div>
+            <IconButton onClick={scrollToBottom} className="md:hidden">
+              <Icon.ArrowThickDownCircle
+                backgroundClass="text-gray-500"
+                foregroundClass="text-gray-600"
+              />
+            </IconButton>
+
+            <Link to={urlWithParams(urls.add, params)}>
+              <IconButton>
+                <Icon.AddSquare
+                  backgroundClass="text-green-300"
+                  foregroundClass="text-green-600"
+                />
+              </IconButton>
+            </Link>
+          </div>
+        </div>
+
+        {filteredTasks.map((task, index) => (
+          // XXX Better index than this? Need to generate and send ID from server?
+          <TaskCard task={task} key={`${index}:${task.raw}`} />
+        ))}
+      </div>
+    </>
   );
 }

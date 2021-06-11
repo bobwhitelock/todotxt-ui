296f4e16196f314530bb9a7804b20c3f8a66567a
Bob Whitelock <bob.whitelock1@gmail.com>
2021-06-11 01:18:37 +0100

    Convert TaskEditor to use Draft.js
    This functions fairly identically to a normal textarea at the moment,
    but this should make if fairly straightforward now to implement
    autocompletion of contexts and projects, as well as various other editor
    features I want to add.

--- client/src/components/TaskEditor.tsx
+++ client/src/components/TaskEditor.tsx
@@ -1,4 +1,6 @@
 import cn from "classnames";
+import { Editor, EditorState, ContentState } from "draft-js";
+import { useState, useRef, useEffect } from "react";
 
 type Props = {
   rawTasks: string;
@@ -6,21 +8,44 @@ type Props = {
 };
 
 export function TaskEditor({ rawTasks, setRawTasks }: Props) {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.moveFocusToEnd(
+      EditorState.createWithContent(ContentState.createFromText(rawTasks))
+    )
+  );
+
+  const editor = useRef<Editor>(null);
+  const focusEditor = () => editor?.current?.focus();
+  // Auto-focus editor on mount.
+  useEffect(focusEditor, []);
+  const [isFocused, setIsFocused] = useState(true);
+
   return (
-    <textarea
+    <div
+      onClick={focusEditor}
       className={cn(
         "flex-grow",
         "w-full",
         "border-4",
-        "border-blue-200",
+        isFocused ? "border-blue-300" : "border-blue-200",
         "border-solid",
+        "bg-white",
         "rounded-lg",
         "md:flex-grow-0",
-        "md:h-64"
+        "md:h-64",
+        "cursor-text"
       )}
-      autoFocus={true}
-      value={rawTasks}
-      onChange={(e) => setRawTasks(e.target.value)}
-    ></textarea>
+    >
+      <Editor
+        ref={editor}
+        editorState={editorState}
+        onChange={(state) => {
+          setEditorState(state);
+          setRawTasks(state.getCurrentContent().getPlainText());
+        }}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+    </div>
   );
 }

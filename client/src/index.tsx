76839e252d907fd0d8bb9b143f13666f69c9af39
Bob Whitelock <bob.whitelock1@gmail.com>
2021-09-30 23:36:40 +0100

    Add Sentry for React frontend (resolves #125)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import "index.css";
 import { App } from "components/App";
 import { reportWebVitals } from "reportWebVitals";
+
+Sentry.init({
+  dsn:
+    process.env.NODE_ENV === "production"
+      ? "https://4de4eb1ce60f4037a7a8febb6ff59100@o383461.ingest.sentry.io/5987296"
+      : "",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set `tracesSampleRate` to 1.0 to capture 100% of transactions for
+  // performance monitoring. Sentry recommend adjusting this value in
+  // production, but possibly this doesn't matter in our case as number of
+  // users will always be very low.
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
